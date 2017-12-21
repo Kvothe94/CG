ae11a9ef3eb7595256a396380d15e958d63eea0f
Unknown <guido9494@gmail.com>
2017-12-21 18:38:05 +0100

    Version 1.5.2
    Modificati valori nella gluLookAt

--- Basic2D/Model.cpp
+++ Basic2D/Model.cpp
@@ -32,7 +32,7 @@ bool MyModel::InitGL(void){
 	// eye    (0,0,0)
 	// center (0,0,-1)
 	// up     (0,1,0)
-	gluLookAt(0.0, 0.0, 0.0,   0.0, 0.0, -1.0,   0.0, 1.0, 0.0);
+	gluLookAt(MAX_X / 2, MAX_Y / 2, EYE_Z,		MAX_X / 2, MAX_Y / 2, 0.0,		0.0, 1.0, 0.0);
 
 	return true;										// Initialization Went OK
 }
